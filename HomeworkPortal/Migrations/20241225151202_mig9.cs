294c98d07f4d4f4a66e2ba3e191da10766c8d6ff
Burak Duran <burak.duran4@gmail.com>
2024-12-25 19:25:02 +0300

    Database update

---
+++ HomeworkPortal/Migrations/20241225151202_mig9.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HomeworkPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig9 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Categories_GradeId",
+                table: "Lessons");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Grades");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Grades",
+                table: "Grades",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Grades_GradeId",
+                table: "Lessons",
+                column: "GradeId",
+                principalTable: "Grades",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lessons_Grades_GradeId",
+                table: "Lessons");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Grades",
+                table: "Grades");
+
+            migrationBuilder.RenameTable(
+                name: "Grades",
+                newName: "Categories");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lessons_Categories_GradeId",
+                table: "Lessons",
+                column: "GradeId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
